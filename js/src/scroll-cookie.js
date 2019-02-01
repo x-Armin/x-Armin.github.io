5543c2b077f0ae345ff658cf522c34c4aa4bb181
x-Armin <1925959375@qq.com>
2019-02-01 15:48:24 +0800

    Site updated: 2019-02-01 15:47:49

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 01 2019 09:37:07 GMT+0800 (中国标准时间)
+// build time:Fri Feb 01 2019 15:45:08 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
